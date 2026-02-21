81a45638a8b0ed3cb4d8c7b2cdc3858d8491a3df
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-02-21 19:30:03 +0530

    multer image uploading backend logic works limit img of 5mb

--- backend/src/routes/uploadRoute.js
+++ backend/src/routes/uploadRoute.js
@@ -1,8 +1,9 @@
-import express from express;
-import { uploadImage } from "../controller/uploadController";
+import express from "express";
+import upload from "../middleware/upload.js";
+import { processImage } from "../controller/uploadController.js";
 
-const router =express.router;
+const router = express.Router();
 
-router.post("/uploads",uploadImage);
+router.post("/analyze", upload.single("image"), processImage);
 
 export default router;

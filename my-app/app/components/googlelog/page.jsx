2e9cfc119d3943e85d921f74a266107571e85fb4
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-05-07 13:48:33 +0530

    fix

--- my-app/app/components/googlelog/page.jsx
+++ my-app/app/components/googlelog/page.jsx
@@ -1,4 +1,5 @@
 // components/GoogleLoginButton.jsx
+"use client";
 export default function GoogleLoginButton() {
   const handleGoogleLogin = () => {
     window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`;
